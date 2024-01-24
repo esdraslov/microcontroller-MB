48eba3d35facb6393dc7346fbcb0f0e66138e126
esdraslov <103470150+esdraslov@users.noreply.github.com>
2024-01-24 10:43:14 -0300

    tests

--- test.ts
+++ test.ts
@@ -1,1 +1,20 @@
 // testes vão aqui; isto não será compilado quando este pacote for usado como uma extensão.
+let paired = false
+radio.setGroup(50)
+basic.forever(() => {
+    if (microController.pairRequested()) {
+        microController.pairAllow(1)
+        paired = true
+    }
+    if (paired) {
+        if (input.buttonIsPressed(Button.A)) {
+            microController.send(microController.Button.A)
+        } else if (input.buttonIsPressed(Button.B)) {
+            microController.send(microController.Button.B)
+        } else if (input.logoIsPressed()) {
+            microController.send(microController.Button.Head)
+        } else {
+            microController.send(microController.Button.None)
+        }
+    }
+})
