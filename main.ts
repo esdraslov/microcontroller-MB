0ccb54bd8c642bce3308c5ea5863a97a24ce0597
esdraslov <103470150+esdraslov@users.noreply.github.com>
2024-01-24 00:09:26 -0300

    make a send function

--- main.ts
+++ main.ts
@@ -7,6 +7,15 @@ radio.onReceivedValue((name, value) => {
 })
 
 namespace microController {
+    export enum Button {
+        //% block="A"
+        A,
+        //% block="B"
+        B,
+        //% block="Head (v2 only)",
+        Head
+    }
+
     //% block="set id to %id%"
     export function setId(id: number) {
         id = id
@@ -21,5 +30,10 @@ namespace microController {
     //% allow.min="0" allow.max="1"
     export function pairAllow(allow: number) {
         radio.sendValue("paired", allow)
+    }
+
+    //% block
+    export function send(button: Button) {
+        radio.sendNumber(button)
     }
 }
